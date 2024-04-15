416f0e8060d53b4b41361ee6631ba4b35a84f93b
alex02r <alex.giochi02@gmail.com>
2024-04-15 20:17:28 +0200

    upgrade: pulsate di login

--- partials/templates/header.php
+++ partials/templates/header.php
@@ -1,12 +1,19 @@
 <header>
     <div class="container">
-        <div class="row justify-content-between py-2">
+        <div class="row justify-content-between align-items-center py-2">
             <div class="col-4 col-md-2">
                 <!-- logo -->
                 <h1 class="text-danger">BLOG</h1>
             </div>
-            <div class="col-6 col-md-8">
+            <div class="col-6 col-md-8 text-end">
                 <!-- menu list -->
+                <?php  
+                    /* Se non siamo loggati visualiziamo il link per il login */
+                ?>
+                    <a class="btn btn-danger" href="#">Login</a>
+                <?php  
+                    /* se loggati visualizziamo il link per gestire i post */
+                ?>
             </div>
         </div>
     </div>
