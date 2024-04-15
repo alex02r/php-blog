e0294af85e0b58676020073dae1e2fefe0e468cb
alex02r <alex.giochi02@gmail.com>
2024-04-15 21:43:31 +0200

    upgrade: struttura html per il login

--- login.php
+++ login.php
@@ -3,13 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- link CSS -->
+     <link rel="stylesheet" href="./css/style.css">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
     <title>Login</title>
 </head>
 <body>
-    <div class="container justify-content-center my-5">
-        <div class="row">
-            <div class="col-6">
-                
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-6 col-md-4">
+                <div class="shadow rounded p-5">
+                    <!-- form di login -->
+                    <form action="#" method="post">
+                        <h2>Login</h2>
+                        <div class="mb-3">
+                            <!-- user -->
+                            <label for="user" class="form-label">Username</label>
+                            <input type="text" id="user" name="user" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <!-- password -->
+                            <label for="psw" class="psw">Password</label>
+                            <input type="text" id="psw" name="psw" class="form-control">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
