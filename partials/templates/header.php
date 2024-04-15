bf2c6a0a6917819e505b9f437ce32e3eae690212
alex02r <alex.giochi02@gmail.com>
2024-04-15 20:09:19 +0200

    upgrade: aggiunta struttura html per l'header

--- partials/templates/header.php
+++ partials/templates/header.php
@@ -1,6 +1,12 @@
 <header>
     <div class="conteainer">
-        <div class="row">
+        <div class="row justify-content-between">
+            <div class="col-4 col-md-2">
+                <!-- logo -->
+            </div>
+            <div class="col-6 col-md-8">
+                <!-- menu list -->
+            </div>
         </div>
     </div>
 </header>
