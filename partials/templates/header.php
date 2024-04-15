c0ead924001968959eb21ce4c38d51fb07362c51
alex02r <alex.giochi02@gmail.com>
2024-04-15 20:11:17 +0200

    upgrade: aggiunto titolo-logo

--- partials/templates/header.php
+++ partials/templates/header.php
@@ -1,8 +1,9 @@
 <header>
-    <div class="conteainer">
-        <div class="row justify-content-between">
+    <div class="container">
+        <div class="row justify-content-between py-2">
             <div class="col-4 col-md-2">
                 <!-- logo -->
+                <h1 class="text-danger">BLOG</h1>
             </div>
             <div class="col-6 col-md-8">
                 <!-- menu list -->
