7be7b6076999f99c0df2b305900426a2da6535b1
alex02r <alex.giochi02@gmail.com>
2024-04-16 18:57:30 +0200

    upgrade: controllo per la modifica

--- show-post.php
+++ show-post.php
@@ -17,15 +17,23 @@
 <body>
     <?php
         session_start(); 
+
+        //controlliamo se la pagina è visualizzata correttamente
+        if (!isset($_GET['id'])) {
+            header('Location: forbidden.php');
+            exit;
+        }
+
         include_once('./partials/templates/header.php'); 
         include('./connection.php');
-
+        
         if (isset($_GET['id'])) {
             $id = $_GET['id'];
+            //preparo la query di selezione
             $query = 'SELECT * FROM posts WHERE id = ?';
             $stmt = $db->prepare($query);
             $stmt->bind_param('s', $id);
-
+            
             //controlliamo l'esecuizione della query
             if ($stmt->execute()) {
                 $result = $stmt->get_result();
@@ -33,9 +41,22 @@
             }else {
                 echo 'errre nell esecuzione della query: '.$stmt->error;
             }
-        }else {
-            header('Location: forbidden.php');
         }
+        //controlliamo la modifca
+        if (isset($_POST['edit'])) {
+            $_SESSION['edit'] = true; 
+            header('Location: show-post.php?id='.$_POST['post_id']);
+            exit;
+        }
+
+        if (isset($_POST['save'])) {
+            unset($_SESSION['edit']);
+            $id = $_POST['post_id'];
+            header('Location: show-post.php?id='.$id);
+            exit;    
+        }
+        
+
     ?>
     <div class="container my-5">
         <div class="row row-gap-4">
@@ -44,7 +65,7 @@
                 if (isset($_SESSION['edit'])) {
                     ?>
                         <div class="col-12 col-md-6">
-                            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                            <form action="<?php echo $_SERVER["PHP_SELF"]."?".$_SERVER['QUERY_STRING']; ?>" method="POST">
                                 <div class="mb-3">
                                     <label for="title">Modifica il titolo: </label>
                                     <input type="text" id="title" name="title" value="<?php echo $post['title']; ?>">
@@ -53,7 +74,8 @@
                                     <label for="content">Modifica la descrizione: </label>
                                     <textarea id="content" name="content" class="form-control" cols="15" rows="5"><?php echo $post['content']; ?></textarea>
                                 </div>
-                                <button type="submit" id="edit" name="edit"  class="btn btn-sm btn-warning">Salva</button>
+                                <input type="hidden" name="post_id" value="<?php echo $post['id']; ?>">
+                                <button type="submit" id="save" name="save"  class="btn btn-sm btn-warning">Salva</button>
                             </form>
                         </div>
                     <?php
@@ -69,7 +91,10 @@
                                 if (isset($_SESSION['user']) && $post['user_id'] == $_SESSION['user']['id']) {
                                     //sei il titolare del post e poi modificarlo
                                     ?>
-                                        <button class="btn btn-sm btn-warning" onclick="<?php $_SESSION['edit'] = true; header('Location: show-post.php?id='.$post['id']);?>">Modifica <i class="fas fa-pen"></i></button>
+                                        <form action="<?php echo $_SERVER["PHP_SELF"]."?".$_SERVER['QUERY_STRING']; ?>" method="POST">
+                                            <input type="hidden" name="post_id" value="<?php echo $post['id']; ?>">
+                                            <button type="submit" id="edit" name="edit" class="btn btn-sm btn-warning">Modifica <i class="fas fa-pen"></i></button>
+                                        </form>
                                     <?php
                                 }
                             ?>
