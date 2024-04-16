06fa1c55a6f22a54039918ac2c860c0e075701b0
alex02r <alex.giochi02@gmail.com>
2024-04-16 10:43:16 +0200

    upgrade: creazione pagina forbidden

---
+++ forbidden.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
+
+    <title>Blog</title>
+</head>
+<body>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 text-center">
+                <h1>403</h1>
+                <h3>Forbidden</h3>
+                <h5>Access to this resorce on the server is denided!</h5>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
